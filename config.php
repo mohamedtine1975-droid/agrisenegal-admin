99ff7885636f0b0f924cc56526fde9f2c6084ee3
mohamedtine1975-droid <mohamedtine1975@gmail.com>
2026-02-26 05:20:43 +0000

    Sécurisation : config.php est maintenant ignoré

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'agrisenegal');
-define('ANTHROPIC_API_KEY', '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');
-?>
