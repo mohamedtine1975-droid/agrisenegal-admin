5f8845683437b22010c8c953b372a87ddda68217
mohamedtine1975-droid <mohamedtine1975@gmail.com>
2026-02-26 05:28:59 +0000

    Sécurisation de la config et ajout du sample

---
+++ config.sample.php
@@ -0,0 +1,13 @@
+<?php
+// Connexion Base de Données
+define('DB_HOST', 'localhost');
+define('DB_USER', 'votre_utilisateur');
+define('DB_PASS', 'votre_mot_de_passe');
+define('DB_NAME', 'agrisenegal');
+
+// Clés API
+define('ANTHROPIC_API_KEY', 'votre_cle_ici');
+
+// Paramètres Site
+define('SITE_URL', 'http://localhost/agrisenegal');
+?>
